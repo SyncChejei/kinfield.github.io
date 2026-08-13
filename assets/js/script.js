f8c2b086eb28df8526eda07f8dbb3e6e04c244bc
chejei <chejei.vallecera@gmail.com>
2026-08-13 23:44:20 +0800

    Style Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+document.querySelectorAll(".accordion-btn").forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const currentContent = button.nextElementSibling;
+        const currentIcon = button.querySelector(".accordion-icon-vertical");
+        const isOpen = button.getAttribute("aria-expanded") === "true";
+
+        // Close all accordions
+        document.querySelectorAll(".accordion-btn").forEach(otherButton => {
+
+            const content = otherButton.nextElementSibling;
+            const vertical = otherButton.querySelector(".accordion-icon-vertical");
+
+            otherButton.setAttribute("aria-expanded", "false");
+
+            content.classList.remove("grid-rows-[1fr]");
+            content.classList.add("grid-rows-[0fr]");
+
+            // Show vertical line = "+"
+            vertical.classList.remove("scale-y-0");
+            vertical.classList.add("scale-y-100");
+        });
+
+        // Open current accordion
+        if (!isOpen) {
+
+            button.setAttribute("aria-expanded", "true");
+
+            currentContent.classList.remove("grid-rows-[0fr]");
+            currentContent.classList.add("grid-rows-[1fr]");
+
+            // Hide vertical line = "−"
+            currentIcon.classList.remove("scale-y-100");
+            currentIcon.classList.add("scale-y-0");
+        }
+
+    });
+
+});
